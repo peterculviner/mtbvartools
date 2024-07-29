ec06ea0e3c6f2855e87d72e57f43bd76eb517a8b
Peter Culviner <peteoburito@gmail.com>
2024-07-29 10:37:50 -0400

    add descendants per time and transmission cluster statistics

--- mtbvartools/trees.py
+++ mtbvartools/trees.py
@@ -1,6 +1,7 @@
 import dendropy
 import numpy as np
 from tqdm import tqdm
+from itertools import product
 import mtbvartools as vt
 
 def writeLabelledTree(input_path=None, output_path=None, input_tree=None, **kwargs):
@@ -69,52 +70,35 @@ def loadTree(nwk_path):
     return tree
 
 
-def getMeanNodeTip(ancestor_path, tree_path, remove_mask, allow_terminal=False):
+def getMeanNodeTip(ancestor_path, tree_path, remove_mask):
     tree = loadTree(tree_path)
-    if allow_terminal:
-        # preload mean node tip allowing terminal branches
-        mean_node_tip = {}
-        with vt.CallBytestream(ancestor_path, init_calls=False, init_nodes=True) as ancestor_calls:
-            for node in tqdm(list(tree.postorder_node_iter())):
-                if node.is_internal():
-                    if node.parent_node == None:
-                        node_tip_distances = []  # root node has no parent
-                    else:
-                        node_tip_distances = [np.mean([  # calculate initial value of parental node to child leaves
-                                getVariantDistance(ancestor_calls, node.parent_node.label, leaf_node.label, remove_mask)
-                                for leaf_node in node.leaf_nodes()])]
-                    for n in node.postorder_iter():
-                        if n != node:
-                            node_tip_distances.append(  # add precalculated child node values to the list
-                                mean_node_tip[n.label])
-                    mean_node_tip[node.label] = np.mean(  # finally calculate the value at this node
-                        node_tip_distances)
-                else:
-                    mean_node_tip[node.label] = getVariantDistance(ancestor_calls, node.parent_node.label, node.label, remove_mask)
-        return mean_node_tip
-    else:
-        # preload mean node tip, excluding terminal branches
-        mean_node_tip_internal = {}
-        with vt.CallBytestream(ancestor_path, init_calls=False, init_nodes=True) as ancestor_calls:
-            for node in tqdm(list(tree.postorder_internal_node_iter())):
-                node_tip_distances = [np.mean([  # calculate initial value of parental node to child leaves
-                    getVariantDistance(ancestor_calls, node.label, leaf_node.label, remove_mask)
-                    for leaf_node in node.leaf_nodes()])]
-                for n in node.postorder_internal_node_iter():
-                    if n != node:
-                        node_tip_distances.append(  # add precalculated child node values to the list
-                            mean_node_tip_internal[n.label])
-                mean_node_tip_internal[node.label] = np.mean(  # finally calculate the value at this node
-                    node_tip_distances)
-        return mean_node_tip_internal
+    # preload mean node tip, excluding terminal branches
+    mean_node_tip_internal = {}
+    with vt.CallBytestream(ancestor_path, init_calls=False, init_nodes=True) as ancestor_calls:
+        for node in tqdm(list(tree.postorder_internal_node_iter())):
+            node_tip_distances = [np.mean([  # calculate initial value of parental node to child leaves
+                getVariantDistance(ancestor_calls, node.label, leaf_node.label, remove_mask)
+                for leaf_node in node.leaf_nodes()])]
+            for n in node.postorder_internal_node_iter():
+                if n != node:
+                    node_tip_distances.append(  # add precalculated child node values to the list
+                        mean_node_tip_internal[n.label])
+            mean_node_tip_internal[node.label] = np.mean(  # finally calculate the value at this node
+                node_tip_distances)
+    return mean_node_tip_internal
 
 
-def getMeanTerminalBranchLengths(ancestor_path, tree_path, remove_mask):
+def getMeanTerminalBranchLengths(ancestor_path, tree_path, remove_mask, allow_leaves=True):
     tree = loadTree(tree_path)
     # mean terminal branch length
     mean_terminal_branch_length = {}
     with vt.CallBytestream(ancestor_path, init_calls=False, init_nodes=True) as ancestor_calls:
-        for node in tqdm(list(tree.postorder_node_iter())):
+        # decide if should call 
+        if allow_leaves:
+            node_iter = tree.postorder_node_iter()
+        else:
+            node_iter = tree.postorder_internal_node_iter()
+        for node in tqdm(list(node_iter)):
             leaf_node_tbls = []
             for ln in node.leaf_nodes():
                 leaf_node_tbls.append(
@@ -122,3 +106,81 @@ def getMeanTerminalBranchLengths(ancestor_path, tree_path, remove_mask):
             mean_terminal_branch_length[node.label] = np.mean(
                 leaf_node_tbls)
     return mean_terminal_branch_length
+
+
+def getDescendantsPerTime(ancestor_path, tree_path, remove_mask, pseudocount=1):
+    tree = loadTree(tree_path)
+    # number of transmission events per time since variant node
+    descendants_per_time = {}
+    with vt.CallBytestream(ancestor_path, init_calls=False, init_nodes=True) as ancestor_calls:
+        for node in tqdm(list(tree.postorder_internal_node_iter())):
+            leaf_distances = [  # calculate variant distances (~= time) to leaf nodes
+                getVariantDistance(ancestor_calls, node.label, leaf_node.label, remove_mask)
+                for leaf_node in node.leaf_nodes()]
+            descendants_per_time[node.label] = len(leaf_distances) / (np.mean(leaf_distances) + pseudocount)
+    return descendants_per_time
+
+
+def getTransmissionClusters(ancestor_path, tree_path, remove_mask, distance):
+    tree = loadTree(tree_path)
+    # identify clusters where ALL strains are in a particular variant distance
+    strains_in_clusters = []
+    cluster_list = []
+    cluster_dict = {}
+    with vt.CallBytestream(ancestor_path, init_calls=False, init_nodes=True) as ancestor_calls:
+        for node in tqdm(list(tree.leaf_nodes())):  # iterate across all leaf nodes
+            if node.label in strains_in_clusters:
+                continue  # clusters are accessible from every node, ignore already clustered strains
+            cluster_set = np.asarray([node.label])
+            test_node = node
+            while node.parent_node is not None:
+                fail_distance = False
+                label_key = test_node.label  # store prior node's label to define the cluster's parent node
+                test_node = test_node.parent_node  # iterate test node <- parent node
+                test_set = np.asarray([n.label for n in test_node.leaf_nodes()])
+                new_leaves = np.setdiff1d(test_set, cluster_set)
+                comparisons = product(  # check if each new leaf is in distance of e/o and old leaves
+                    new_leaves, test_set)
+                for leaf1, leaf2 in comparisons:
+                    if getVariantDistance(ancestor_calls, leaf1, leaf2, remove_mask) > distance:
+                        fail_distance = True  # at least one strain fails distance
+                        break
+                if fail_distance:
+                    break  # break while loop
+                else:  # current set is passing
+                    cluster_set = test_set
+            if len(cluster_set) > 1:
+                cluster_list.append(  # add cluster to flat list
+                    cluster_set)
+                strains_in_clusters = np.concatenate(  # add cluster strains to list of strains in SOME cluster
+                    (strains_in_clusters, cluster_set))
+                # store cluster keyed by defining parental node
+                if label_key in cluster_dict.keys():
+                    raise IndexError('Clusters should only be defined once - something is wrong!')
+                cluster_dict[label_key] = cluster_set
+    return cluster_dict, cluster_list, strains_in_clusters
+
+
+def getChildClusterFraction(tree_path, cluster_dict):
+    tree = loadTree(tree_path)
+    # for each node, get the fraction of strains that are in clusters completely child to node
+    clustered_fraction = {}
+    defining_nodes = list(cluster_dict.keys())
+    for target_node in tqdm(list(tree.postorder_internal_node_iter())):
+        if target_node.label in defining_nodes:  # easy case, node defines cluster -> 100% clustered
+            clustered_fraction[target_node.label] = 1
+        else:  # need to do a calculation
+            internal_child_nodes = [  # get all internal child nodes
+                n.label for n in target_node.postorder_internal_node_iter()]
+            cluster_child_nodes = np.intersect1d(  # get child nodes defining a cluster
+                defining_nodes, internal_child_nodes)
+            if len(cluster_child_nodes) == 0:  # no clusters found -> 0% clustered
+                clustered_fraction[target_node.label] = 0
+            else:
+                # identify leaves that fall within a cluster
+                clustered_leaves = []
+                for cluster_node in cluster_child_nodes:
+                    clustered_leaves += [ln.label for ln in tree.find_node_with_label(cluster_node).leaf_nodes()]
+                # compare to total number of child leaves
+                clustered_fraction[target_node.label] = len(clustered_leaves) / len(target_node.leaf_nodes())
+    return clustered_fraction
